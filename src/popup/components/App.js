f133f17b5f82e797b60ac1589c3106b8c1444737
alex-pex <alexandre.paixao@stadline.com>
2018-05-13 23:32:10 +0200

    Use Bootstrap 4, create a UI

--- src/popup/components/App.js
+++ src/popup/components/App.js
@@ -1,20 +1,111 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
-import './App.css';
-import Logo from './Logo';
+import {
+  Button,
+  Collapse,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Nav,
+  NavItem,
+  NavLink,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Progress,
+  Table,
+  UncontrolledDropdown,
+} from 'reactstrap';
 
 const App = ({ links }) => (
-  <div className="App">
-    <header className="App-header">
-      <Logo />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-    <ul>{links.map(url => <li key={url}>{url}</li>)}</ul>
-  </div>
+  <Fragment>
+    <Navbar color="success" dark>
+      <NavbarBrand href="/">
+        <strong>Download</strong>Station Companion
+      </NavbarBrand>
+      <NavbarToggler />
+      <Collapse navbar>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink href="/components/">Components</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+          </NavItem>
+          <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+              Options
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem>Option 1</DropdownItem>
+              <DropdownItem>Option 2</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Reset</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </Nav>
+      </Collapse>
+    </Navbar>
+    <Table small className="no-margin-bottom">
+      <thead className="thead-light">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Source/Name</th>
+          <th scope="col">Progress</th>
+          <th scope="col">#</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">
+            <input type="checkbox" />
+          </th>
+          <td>Mass Effect - Andromeda [FitGirl Repack]</td>
+          <td>
+            <Progress animated value={100} color="info">
+              Seeding...
+            </Progress>
+          </td>
+          <td>
+            <UncontrolledDropdown direction="left">
+              <DropdownToggle caret>Dropdown</DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem header>Header</DropdownItem>
+                <DropdownItem disabled>Action</DropdownItem>
+                <DropdownItem>Another Action</DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>Another Action</DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </td>
+        </tr>
+        {links.map(url => (
+          <tr key={url}>
+            <th scope="row">
+              <input type="checkbox" />
+            </th>
+            <td>{url}</td>
+            <td>
+              <Progress value={50} />
+            </td>
+            <td>
+              <UncontrolledDropdown direction="left">
+                <DropdownToggle caret>Dropdown</DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem header>Header</DropdownItem>
+                  <DropdownItem disabled>Action</DropdownItem>
+                  <DropdownItem>Another Action</DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>Another Action</DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  </Fragment>
 );
 
 App.propTypes = {
--- src/popup/components/Logo.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './Logo.css';
-import logo from './logo.svg';
-
-const Logo = () => <img src={logo} className="Logo" alt="App logo" />;
-
-export default Logo;
